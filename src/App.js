d5f22f225c9c35e8a93afea29a729c871f0d22cd
cw-barry <barry@clarusway.com>
2022-10-11 19:41:31 -0400

    add handlers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+import dataFromFile from './helpers/data';
+
 function App() {
+  const [data, setData] = useState(dataFromFile);
+  const [people, setPeople] = useState(data.slice(0, 5));
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    let newIndex = index + 5;
+    if (newIndex > data.length - 1) newIndex = 0;
+    setIndex(newIndex);
+    setPeople(data.slice(newIndex, newIndex + 5));
+  };
+  const handlePrev = () => {
+    let newIndex = index - 5;
+    if (newIndex < 0) newIndex = data.length - 5;
+    setIndex(newIndex);
+    setPeople(data.slice(newIndex, newIndex + 5));
+  };
   return (
     <div className="team-boxed">
       <div className="container">
